e43bd1488b715bb86fc9561eb95eb816c4ac70a4
alonso fabila <jorge.fabila@encora.com>
2024-09-09 14:23:47 -0600

    (chore): use amadeus api instead of mocked data.

--- BackendFlightSearch/src/main/java/com/encoramx/backendflightsearch/integrations/AmadeusFlightsAPIIntegration.java
+++ BackendFlightSearch/src/main/java/com/encoramx/backendflightsearch/integrations/AmadeusFlightsAPIIntegration.java
@@ -4,18 +4,15 @@ import com.encoramx.backendflightsearch.records.AmadeusAccessInfo;
 import com.encoramx.backendflightsearch.records.SearchFilters;
 import com.encoramx.backendflightsearch.records.airportandcityapi.AirlineAndCityResponseAPI;
 import com.encoramx.backendflightsearch.records.flightofferssearchapi.FlightOffersResponseApi;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.Objects;
 
 @Component
@@ -65,72 +62,100 @@ public class AmadeusFlightsAPIIntegration {
     }
 
 
-//    public AirlineAndCityResponseAPI getAirports(String city) {
-//        String endpoint = "/v1/reference-data/locations" +
-//                "?subType=CITY" +
-//                "&keyword=" + city +
-//                "&page[limit]=5" +
-//                "&page[offset]=0" +
-//                "&sort=analytics.travelers.score" +
-//                "&view=LIGHT";
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Authorization", "Bearer " + getAccessToken());
-//        HttpEntity<String> entity = new HttpEntity<>(headers);
-//
-//        String url = "https://test.api.amadeus.com" + endpoint;
-//
-//        ResponseEntity<AirlineAndCityResponseAPI> response = restTemplate.exchange(url, HttpMethod.GET, entity, AirlineAndCityResponseAPI.class);
-//
-//        return response.getBody();
-//    }
-
-
-    // quitar dependencia despues     implementation 'com.fasterxml.jackson.core:jackson-databind:2.15.0'
     public AirlineAndCityResponseAPI getAirports(String city) {
-        try {
-            ClassPathResource resource = new ClassPathResource("static/mockAirportAndCityResponse.json");
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(resource.getFile(), AirlineAndCityResponseAPI.class);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load mock flight offers data", e);
-        }
+        String endpoint = "/v1/reference-data/locations" +
+                "?subType=CITY" +
+                "&keyword=" + city +
+                "&page[limit]=5" +
+                "&page[offset]=0" +
+                "&sort=analytics.travelers.score" +
+                "&view=LIGHT";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + getAccessToken());
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        String url = "https://test.api.amadeus.com" + endpoint;
+
+        ResponseEntity<AirlineAndCityResponseAPI> response = restTemplate.exchange(url, HttpMethod.GET, entity, AirlineAndCityResponseAPI.class);
+
+        return response.getBody();
     }
 
-//    public FlightOffersResponseApi getFlightOffers(SearchFilters searchFilter) {
-//        String endpoint = "/v2/shopping/flight-offers" +
-//                "?originLocationCode=" + searchFilter.departureCity() +
-//                "&destinationLocationCode=" + searchFilter.arrivalCity() +
-//                "&departureDate=" + searchFilter.departureDate() +
-//                "&returnDate=" + searchFilter.returnDate() +
-//                "&adults=" + searchFilter.travelers().adults() +
-//                "&children=" + searchFilter.travelers().children() +
-//                "&infants=" + searchFilter.travelers().infants() +
-//                "&nonStop=" + searchFilter.nonStop() +
-//                "&currencyCode=" + searchFilter.currency();
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.set("Authorization", "Bearer " + getAccessToken());
-//        HttpEntity<String> entity = new HttpEntity<>(headers);
-//
-//        String url = "https://test.api.amadeus.com" + endpoint;
-//
-//        ResponseEntity<FlightOffersResponseApi> response = restTemplate.exchange(url, HttpMethod.GET, entity, FlightOffersResponseApi.class);
-//
-//        return response.getBody();
-//    }
 
-
-    // quitar dependencia despues  implementation 'com.fasterxml.jackson.core:jackson-databind:2.15.0'
     public FlightOffersResponseApi getFlightOffers(SearchFilters searchFilter) {
-        try {
-            ClassPathResource resource = new ClassPathResource("static/mockFlightOffersResponse.json");
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(resource.getFile(), FlightOffersResponseApi.class);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load mock flight offers data", e);
+        String endpoint;
+
+        if (searchFilter.returnDate().isEmpty()) {
+            endpoint = "/v2/shopping/flight-offers" +
+                    "?originLocationCode=" + searchFilter.departureCity() +
+                    "&destinationLocationCode=" + searchFilter.arrivalCity() +
+                    "&departureDate=" + searchFilter.departureDate() +
+                    "&adults=" + searchFilter.travelers().adults() +
+                    "&children=" + searchFilter.travelers().children() +
+                    "&infants=" + searchFilter.travelers().infants() +
+                    "&nonStop=" + searchFilter.nonStop() +
+                    "&currencyCode=" + searchFilter.currency() +
+                    "&max=50";
+        } else {
+            endpoint = "/v2/shopping/flight-offers" +
+                    "?originLocationCode=" + searchFilter.departureCity() +
+                    "&destinationLocationCode=" + searchFilter.arrivalCity() +
+                    "&departureDate=" + searchFilter.departureDate() +
+                    "&returnDate=" + searchFilter.returnDate() +
+                    "&adults=" + searchFilter.travelers().adults() +
+                    "&children=" + searchFilter.travelers().children() +
+                    "&infants=" + searchFilter.travelers().infants() +
+                    "&nonStop=" + searchFilter.nonStop() +
+                    "&currencyCode=" + searchFilter.currency() +
+                    "&max=50";
         }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + getAccessToken());
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        String url = "https://test.api.amadeus.com" + endpoint;
+
+        ResponseEntity<FlightOffersResponseApi> response = restTemplate.exchange(url, HttpMethod.GET, entity, FlightOffersResponseApi.class);
+
+        return response.getBody();
     }
 
-
+//
+//        public TransformedAPIResponse sendFlightOffers(FlightOffersResponseApi flightOffersResponse) {
+//        Map<String, String> carriers = flightOffersResponse.dictionaries().carriers();
+//
+//        List<FlightOffers> flightOffers = flightOffersResponse.data().stream().map(flightOffer -> {
+//            boolean oneWay = flightOffer.oneWay();
+//
+//            Itineraries itineraries = new Itineraries(
+//                    flightOffer.itineraries().getFirst().duration(),
+//                    flightOffer.itineraries().getFirst().segments().stream().map(segment ->
+//                        new Segments(
+//                                segment.departure(),
+//                                segment.arrival(),
+//                                segment.carrierCode(),
+//                                segment.duration()
+//                        )
+//                    ).toList()
+//            );
+//
+//            Pricing pricing = new Pricing(
+//                    flightOffer.price().currency(),
+//                    flightOffer.price().grandTotal(),
+//                    flightOffer.travelerPricings().stream().map(travelerPricing ->
+//                            new TravelersPricing(
+//                                    travelerPricing.travelerType(),
+//                                    travelerPricing.price().total()
+//                            )
+//                    ).toList()
+//            );
+//
+//            return new FlightOffers(oneWay, itineraries, pricing);
+//
+//        }).toList();
+//
+//        return new TransformedAPIResponse(carriers, flightOffers);
+//    }
 }
